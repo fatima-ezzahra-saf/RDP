3e5f31953318331a401843df00825eb56e6ba331
fatima_ezzahra <fatimaezzahrasafrani@gmail.com>
2024-05-16 09:06:09 +0000

    V1

---
+++ src/Serveur/TestServeur.java
@@ -0,0 +1,10 @@
+package Serveur;
+
+public class TestServeur {
+    public static void main(String[] args) {
+        CodeAuth frame1=new CodeAuth();
+        frame1.setSize(400,120);
+        frame1.setLocation(500,300);
+        frame1.setVisible(true);
+    }
+}
